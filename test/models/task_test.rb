cf625073abb10e5428b45bf25611646d0072ad89
Alex Buschle <alexwbuschle@gmail.com>
2016-01-18 20:49:19 -0200

    TDD task model - should be invalid without some attributes

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should be invalid' do
+    task = Task.new
+    assert task.invalid?
+    assert task.errors.include?(:name)
+    assert task.errors.include?(:user)
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should be invalid' do
+    user = User.new
+    assert task.invalid?
+    assert user.errors.include?(:email)
+    assert user.errors.include?(:password)
+  end
+
+  test 'should be valid' do
+    user = User.new(email: 'foo@bar.com', password: 'randompassword')
+    assert user.valid?
+  end
 end
